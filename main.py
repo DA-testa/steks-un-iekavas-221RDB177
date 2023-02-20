c9dce544fc79155964c942434535730b4c9b9a17
221RDB177 <125874315+221RDB177@users.noreply.github.com>
2023-02-20 12:40:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#Jekaterīna Kondrašova 221RDB177
 # python3
 
 from collections import namedtuple
@@ -10,22 +11,42 @@ def are_matching(left, right):
 
 
 def find_mismatch(text):
-    opening_brackets_stack = []
+    op_br_stack = []
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
             pass
-
+op_br_stack.append(next)
         if next in ")]}":
             # Process closing bracket, write your code here
             pass
-
-
+if((len(op_br_stack) == 0) or not (are_matching(op_br_stack[-1], next))):
+    return i+1
+op_br_stack.pop()
+return 0
 def main():
     text = input()
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
-
+choice = input ("choose your input: ")
+if "F" in choice:
+    path = input("enter path to files: ")
+    with open(path, "R") as  file:
+        text = file.read()
+        mismatch = find_mismatch(text)
+        if mismatch ==  0:
+            print("Success")
+            else: 
+                print(mismatch)
+                elif "I" in choice:
+                    text = input()
+                    mismatch = find.mismatch(text)
+                    if mismatch == 0:
+                        print("Success")
+                        else:
+                            print(mismatch)
+                            else:
+                                print("invalid input")
 
 if __name__ == "__main__":
     main()

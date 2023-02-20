cfe324d4feb000d8eca263c18e5cb0a5e4cdb9f8
221RDB177 <125874315+221RDB177@users.noreply.github.com>
2023-02-20 12:53:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def find_mismatch(text):
         if next in ")]}":
             # Process closing bracket, write your code here
             pass
-if((len( opening_brackets_stack) == 0) or not (are_matching( opening_brackets_stack[-1], next))):
+if((len(opening_brackets_stack) == 0) or not (are_matching(opening_brackets_stack[-1], next))):
     return i+1
  opening_brackets_stack.pop()
 return 0
